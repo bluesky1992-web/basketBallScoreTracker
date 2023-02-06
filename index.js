ecc38ab8e570efca54e9165c7ccd4b455affbab6
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-02-06 18:38:58 +0300

    finished the project and makes it functioning

--- index.js
+++ index.js
@@ -4,30 +4,43 @@ let guestCounter = 0;
 
 const homeCountOutput = document.getElementById("homeCount");
 const guestCounterOutput = document.getElementById("guestCount");
-
-
+const newBut = document.getElementsById("my-Button1");
 
 function reset() {
-    homeCountOutput.textContent = homeCounter;
-guestCounterOutput.textContent = guestCounter
+  homeCountOutput.textContent = homeCounter;
+  guestCounterOutput.textContent = guestCounter;
 }
 
-reset()
-
+reset();
 
 function homeFunc(value) {
   homeCounter += value;
   homeCountOutput.textContent = homeCounter;
-  reset()
+  leaderIndicator();
+  reset();
 }
 
 function guestFunc(value) {
-guestCounter += value
- guestCounterOutput.textContent = guestCounter
- console.log(guestCounter)
- reset()
+  guestCounter += value;
+  guestCounterOutput.textContent = guestCounter;
+  leaderIndicator();
+  reset();
 }
 
+function resetButton() {
+  homeCounter = 0;
+  guestCounter = 0;
+  leaderIndicator();
+  reset();
+}
 
-
-
+function leaderIndicator() {
+  if (homeCounter > guestCounter) {
+    homeCountOutput.classList.add("highlight");
+  } else if (homeCounter < guestCounter) {
+    guestCounterOutput.classList.add("highlight"); 
+  } else {
+    homeCountOutput.classList.remove("highlight");
+    guestCounterOutput.classList.remove("highlight");
+  }
+}
